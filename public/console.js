56dcaccb05878664ed58a636adb873beb852184b
Trevor Thalacker <hello@trevorthalacker.com>
2021-05-23 16:50:41 -0700

    :art: Lint code

--- public/console.js
+++ public/console.js
@@ -120,52 +120,57 @@
  /*********
  /*********************************************************************************************************************/
 
-    let cmdHistory = [];
+let cmdHistory = []
 
-    const start = () => {
-        console.log(`You found a secret!`);
-        console.log(`%cImportant: If you are going to continue, just note: viewing the source of this website is cheating`, 'color:red;')
-        console.log(`Would you like to play a game? [Yes/No]`)
+const start = () => {
+    console.log(`You found a secret!`)
+    console.log(
+        `%cImportant: If you are going to continue, just note: viewing the source of this website is cheating`,
+        'color:red;'
+    )
+    console.log(`Would you like to play a game? [Yes/No]`)
+}
+
+const Yes = () => {
+    // console.log(`Let's play a game then. Are you ready?`) For the future
+    console.log(
+        `I like you. Sadly, I haven't came up with a good plot for this game, so it remains unfinished.`
+    )
+    return console.log(
+        `If you've got a good idea for a plot, let me know on Twitter (@TrevorThalacker) `
+    )
+}
+
+const No = () => {
+    console.log(`The only winning move is not to play.`)
+}
+
+const RegisterCommands = (command, synonyms, hasSetter = false) => {
+    const evaluate = (cmd, args) => {
+        cmdHistory.push(cmd)
+        return command.apply(this, args)
     }
 
-    const Yes = () => {
-        // console.log(`Let's play a game then. Are you ready?`) For the future
-        console.log(`I like you. Sadly, I haven't came up with a good plot for this game, so it remains unfinished.`)
-        return console.log(`If you've got a good idea for a plot, let me know on Twitter (@TrevorThalacker) `)
-    }
-
-    const No = () => {
-        console.log(`The only winning move is not to play.`)
-    }
-
-    const RegisterCommands = (command, synonyms, hasSetter = false) => {
-        const evaluate = (cmd, args) => {
-            cmdHistory.push(cmd)
-            return command.apply(this, args)
-        }
-
-        synonyms.split(',').forEach((cmd) => {
-            if (window.hasOwnProperty(cmd)) {
-                return '';
-            } else {
-                var obj = {
-                    get: function() {
-                        return evaluate(cmd, arguments)
-                    },
-                    set: function() {
-
-                    }
-                }
-                if (hasSetter) {
-                    obj.set = function() {
-                        return console.log(evaluate(cmd, arguments))
-                    }
-                }
-                Object.defineProperty(window, cmd, obj)
+    synonyms.split(',').forEach((cmd) => {
+        if (window.hasOwnProperty(cmd)) {
+            return ''
+        } else {
+            var obj = {
+                get: function () {
+                    return evaluate(cmd, arguments)
+                },
+                set: function () {},
             }
-        })
-    }
+            if (hasSetter) {
+                obj.set = function () {
+                    return console.log(evaluate(cmd, arguments))
+                }
+            }
+            Object.defineProperty(window, cmd, obj)
+        }
+    })
+}
 
-    RegisterCommands(Yes, 'Yes,yes,y,Y', true)
-    RegisterCommands(No, 'No,no,n,N')
-    start()
+RegisterCommands(Yes, 'Yes,yes,y,Y', true)
+RegisterCommands(No, 'No,no,n,N')
+start()
