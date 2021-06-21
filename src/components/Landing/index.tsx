ad16adbfcb458066e76ea1c5da0b6e2e681305d9
Trevor Thalacker <hello@trevorthalacker.com>
2021-06-20 19:50:02 -0700

    :sparkles: Add animated background to landing page

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { AnimatedBackground } from '../Animated'
 
 import { StyledLanding, HeaderText, SubheaderText } from './style'
 
 export const Landing = ({ children }: { children: any }) => {
     return (
         <StyledLanding>
+            <AnimatedBackground />
             <div>{children}</div>
         </StyledLanding>
     )
