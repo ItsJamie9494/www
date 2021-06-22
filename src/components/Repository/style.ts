aad289c2f09cf5f8b2d927eb9aede8772dae2bd5
Trevor Thalacker <hello@trevorthalacker.com>
2021-06-21 22:11:33 -0700

    :lipstick: WIP: revert a bunch of repo stuff

--- src/components/Repository/style.ts
+++ src/components/Repository/style.ts
@@ -41,8 +41,6 @@ export const HeaderContainer = styled.span`
     text-overflow: ellipsis;
     white-space: nowrap;
     overflow: hidden;
-    display: flex;
-    align-items: center;
 `
 
 export const HeaderText = styled.p`
