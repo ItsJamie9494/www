d03cbc0cc930011dee38987d85a55c0f166f34ee
Trevor Thalacker <hello@trevorthalacker.com>
2021-07-21 16:44:30 -0700

    :fire: Remove all the code single page stuff

--- src/components/ProjectsPage/style.ts
+++
@@ -1,9 +0,0 @@
-import styled, { css } from 'styled-components'
-
-export const ProjectsGrid = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    gap: 20px;
-    justify-content: center;
-    margin-top: 2rem;
-`
