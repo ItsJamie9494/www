258e8af1590f1b246fd0e5e9cf2fa80963ea121a
Trevor Thalacker <hello@trevorthalacker.com>
2021-07-22 16:18:52 -0700

    :bug: Animated background should never reload

--- src/components/Animated/index.tsx
+++ src/components/Animated/index.tsx
@@ -2,100 +2,107 @@ import React from 'react'
 import { StyledAnimatedBackground } from './style'
 import { AnimatedSVG } from './SVG'
 
-export const AnimatedBackground = () => {
-    return (
-        <StyledAnimatedBackground>
-            <>
-                {/* NORMAL ANIMATION */}
-                <AnimatedSVG
-                    icon="triangle"
-                    width={48}
-                    stroke
-                    left={`${Math.floor(Math.random() * 101)}%`}
-                    top={`${Math.floor(Math.random() * 101)}%`}
-                />
-                <AnimatedSVG
-                    icon="hexa"
-                    width={48}
-                    stroke
-                    left={`${Math.floor(Math.random() * 101)}%`}
-                    top={`${Math.floor(Math.random() * 101)}%`}
-                />
-                <AnimatedSVG
-                    icon="box"
-                    width={6}
-                    left={`${Math.floor(Math.random() * 80)}%`}
-                    top={`${Math.floor(Math.random() * 80)}%`}
-                    horizontal={Math.random() < 0.5}
-                />
-            </>
+export class AnimatedBackground extends React.Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        // component should never reload, it's supposed to stay the same no matter what
+        return false
+    }
 
-            <>
-                {/* WIDE ANIMATION */}
-                <AnimatedSVG
-                    icon="triangle"
-                    width={24}
-                    stroke
-                    left={`${Math.floor(Math.random() * 101)}%`}
-                    top={`${Math.floor(Math.random() * 101)}%`}
-                    animation="wide"
-                />
-                <AnimatedSVG
-                    icon="triangle"
-                    width={12}
-                    stroke
-                    left={`${Math.floor(Math.random() * 80)}%`}
-                    top={`${Math.floor(Math.random() * 80)}%`}
-                    animation="wide"
-                    horizontal={Math.random() < 0.5}
-                />
-                <AnimatedSVG
-                    icon="triangle"
-                    width={16}
-                    stroke
-                    left={`${Math.floor(Math.random() * 80)}%`}
-                    top={`${Math.floor(Math.random() * 80)}%`}
-                    animation="wide"
-                    horizontal={Math.random() < 0.5}
-                />
-            </>
+    render() {
+        return (
+            <StyledAnimatedBackground>
+                <>
+                    {/* NORMAL ANIMATION */}
+                    <AnimatedSVG
+                        icon="triangle"
+                        width={48}
+                        stroke
+                        left={`${Math.floor(Math.random() * 101)}%`}
+                        top={`${Math.floor(Math.random() * 101)}%`}
+                    />
+                    <AnimatedSVG
+                        icon="hexa"
+                        width={48}
+                        stroke
+                        left={`${Math.floor(Math.random() * 101)}%`}
+                        top={`${Math.floor(Math.random() * 101)}%`}
+                    />
+                    <AnimatedSVG
+                        icon="box"
+                        width={6}
+                        left={`${Math.floor(Math.random() * 80)}%`}
+                        top={`${Math.floor(Math.random() * 80)}%`}
+                        horizontal={Math.random() < 0.5}
+                    />
+                </>
 
-            <>
-                {/* SLOW ANIMATION */}
-                <AnimatedSVG
-                    icon="circle"
-                    width={20}
-                    hiddenMobile
-                    left={`${Math.floor(Math.random() * 101)}%`}
-                    top={`${Math.floor(Math.random() * 101)}%`}
-                    animation="slow"
-                />
-                <AnimatedSVG
-                    icon="circle"
-                    hiddenMobile
-                    stroke
-                    width={24}
-                    left={`${Math.floor(Math.random() * 101)}%`}
-                    top={`${Math.floor(Math.random() * 101)}%`}
-                    animation="slow"
-                />
-                <AnimatedSVG
-                    icon="circle"
-                    width={6}
-                    left={`${Math.floor(Math.random() * 80)}%`}
-                    top={`${Math.floor(Math.random() * 80)}%`}
-                    animation="slow"
-                    horizontal={Math.random() < 0.5}
-                />
-                <AnimatedSVG
-                    icon="circle"
-                    width={12}
-                    left={`${Math.floor(Math.random() * 80)}%`}
-                    top={`${Math.floor(Math.random() * 80)}%`}
-                    animation="slow"
-                    horizontal={Math.random() < 0.5}
-                />
-            </>
-        </StyledAnimatedBackground>
-    )
+                <>
+                    {/* WIDE ANIMATION */}
+                    <AnimatedSVG
+                        icon="triangle"
+                        width={24}
+                        stroke
+                        left={`${Math.floor(Math.random() * 101)}%`}
+                        top={`${Math.floor(Math.random() * 101)}%`}
+                        animation="wide"
+                    />
+                    <AnimatedSVG
+                        icon="triangle"
+                        width={12}
+                        stroke
+                        left={`${Math.floor(Math.random() * 80)}%`}
+                        top={`${Math.floor(Math.random() * 80)}%`}
+                        animation="wide"
+                        horizontal={Math.random() < 0.5}
+                    />
+                    <AnimatedSVG
+                        icon="triangle"
+                        width={16}
+                        stroke
+                        left={`${Math.floor(Math.random() * 80)}%`}
+                        top={`${Math.floor(Math.random() * 80)}%`}
+                        animation="wide"
+                        horizontal={Math.random() < 0.5}
+                    />
+                </>
+
+                <>
+                    {/* SLOW ANIMATION */}
+                    <AnimatedSVG
+                        icon="circle"
+                        width={20}
+                        hiddenMobile
+                        left={`${Math.floor(Math.random() * 101)}%`}
+                        top={`${Math.floor(Math.random() * 101)}%`}
+                        animation="slow"
+                    />
+                    <AnimatedSVG
+                        icon="circle"
+                        hiddenMobile
+                        stroke
+                        width={24}
+                        left={`${Math.floor(Math.random() * 101)}%`}
+                        top={`${Math.floor(Math.random() * 101)}%`}
+                        animation="slow"
+                    />
+                    <AnimatedSVG
+                        icon="circle"
+                        width={6}
+                        left={`${Math.floor(Math.random() * 80)}%`}
+                        top={`${Math.floor(Math.random() * 80)}%`}
+                        animation="slow"
+                        horizontal={Math.random() < 0.5}
+                    />
+                    <AnimatedSVG
+                        icon="circle"
+                        width={12}
+                        left={`${Math.floor(Math.random() * 80)}%`}
+                        top={`${Math.floor(Math.random() * 80)}%`}
+                        animation="slow"
+                        horizontal={Math.random() < 0.5}
+                    />
+                </>
+            </StyledAnimatedBackground>
+        )
+    }
 }
