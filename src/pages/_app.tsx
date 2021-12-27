bb0db7d7b34e519db4c76565960bd8b260b840b5
Jamie Thalacker <hello@jamiethalacker.dev>
2021-12-26 17:10:33 -0800

    :sparkles: Create scroller

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from 'next/app'
 import '../style/globals.css'
+import scroll from 'react-scroll'
+
+export const scroller = scroll.scroller
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
